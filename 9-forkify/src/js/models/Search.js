90d3d3f50b064cf9569741b1714744fc0f78a09d
KoshiroKun <kuroudo@protonmail.com>
2019-07-24 18:09:03 +0200

    Implement search module

---
+++ 9-forkify/src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = 'da085575bc27f7a7aebd001681b159ac';
+        const cors_proxy = 'http://cors-anywhere.herokuapp.com/';
+        try {
+            const result = await axios(`${cors_proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.recipes = result.data.recipes;
+            console.log(recipes);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    }
+}
