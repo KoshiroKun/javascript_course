07b7a2a79fa0faeafdece8ae7dcfac7ccd5628c0
KoshiroKun <kuroudo@protonmail.com>
2019-07-25 18:17:46 +0200

    clean recipes and search module

--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -2,6 +2,31 @@ import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+    elements.searchResultList.innerHTML = '';
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((accumulator, current) => {
+            if (accumulator + current.length <= limit) {
+                newTitle.push(current);
+            }
+            return accumulator + current.length;
+        }, 0);
+
+        // Return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -10,7 +35,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
@@ -18,8 +43,8 @@ const renderRecipe = recipe => {
     `;
 
     elements.searchResultList.insertAdjacentHTML('beforeend', markup);
-}
+};
 
 export const renderResults = recipes => {
     recipes.forEach(renderRecipe);
-}
+};
