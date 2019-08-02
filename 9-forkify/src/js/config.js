d8f1c27f3e59187c3cdd7e24df4edb3ddd86a604
KoshiroKun <kuroudo@protonmail.com>
2019-08-02 13:46:22 +0200

    fixed http mixed content

--- 9-forkify/src/js/config.js
+++ 9-forkify/src/js/config.js
@@ -1,4 +1,4 @@
 const key = ['da085575bc27f7a7aebd001681b159ac', 'dbd04a9b847363f7b23d65c961f2d1d6'];
-export const cors_proxy = 'http://cors-anywhere.herokuapp.com/';
+export const cors_proxy = 'https://cors-anywhere.herokuapp.com/';
 
 export const getKey = () => key[1];
